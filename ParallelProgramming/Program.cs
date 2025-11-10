7208eb7ed6be4abd35db83d5ddab8453858430d5
she <lxst@list.ru>
2025-11-10 16:21:36 +0500

    Добавьте файлы проекта.

---
+++ ParallelProgramming/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming
+{
+    internal class Program
+    {
+        static int[] GetAndFillArray(object userInput)
+        {
+            int temp = (int)userInput;
+            Random random = new Random();
+            int[] userArray = new int[temp];
+
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                userArray[i] = random.Next(0, 100);
+            }
+
+            Array.Sort(userArray);
+            return userArray;
+        }
+
+        static void PrintArray(object userArray)
+        {
+            int[] temp = (int[]) userArray;
+            Console.WriteLine("Массив выглядит следующим образом: ");
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.Write(temp[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static int sumOfArray(object userArray)
+        {
+            int[] temp = (int[]) userArray;
+            if (temp.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                sum += temp[i];
+            }
+
+            return sum;
+        }
+
+        static int maxOfArray(object userArray)
+        {
+            int[] temp = (int[]) userArray;
+            if (temp.Length == 0)
+            {
+                return 0;
+            }
+
+            int max = temp[0];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] > max)
+                {
+                    max = temp[i];
+                }
+            }
+            
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество массива: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int[] userArray = new int[] { };
+
+            Func<object, int[]> firstFunc = new Func<object, int[]>(GetAndFillArray);
+            Task<int[]> firstTask = new Task<int[]>(firstFunc, userInput);
+            firstTask.Start();
+
+            Action<Task<int[]>> firstAction = new Action<Task<int[]>>(PrintArray);
+            Task secondTask = firstTask.ContinueWith(PrintArray);
+
+            Func<Action<Task<int[]>>, int> secondFunc = new Func<Action<Task<int[]>>, int>(sumOfArray);
+            Task<int> thirdTask = secondTask.ContinueWith(sumOfArray);
+            int sum = thirdTask.Result;
+
+            Func<Action<Task<int[]>>, int> thirdFunc = new Func<Action<Task<int[]>>, int> (maxOfArray);
+            Task<int> fourthTask = thirdTask.ContinueWith(maxOfArray);
+            int max = fourthTask.Result;
+
+            Task.WhenAll(secondTask, thirdTask, fourthTask);
+
+            Console.WriteLine($"Сумма массива - {sum}, самое большое число в массиве {max}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParallelProgramming/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ParallelProgramming")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParallelProgramming")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("158ab3aa-5314-47d4-bdf9-905c777a5c31")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
